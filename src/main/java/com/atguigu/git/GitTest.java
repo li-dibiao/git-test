8f63fd727ff897369fbd5c1f37f1f6b6c4c70911
Lidibiao <1534860984@qq.com>
2023-01-01 12:04:54 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @Author li-dibiao
+ * @Date 2023/1/1 12:01
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
